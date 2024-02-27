3a2b80b839f19d78ccf57eab9d53e0f94d49e910
Afridi <afridiwhatever@gmail.com>
2024-02-27 19:25:37 +0600

    working on media access

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,4 +1,19 @@
+import { User } from "./../payload/payload-types";
 import { CollectionConfig } from "payload/types";
+import { Access } from "payload/types";
+
+const isAdminOrHasAccessToImages = (): Access => {
+  return async ({ req }) => {
+    const user = req.user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    return {
+      user: { equals: req.user.id },
+    };
+  };
+};
 
 export const Media: CollectionConfig = {
   slug: "media",
@@ -51,5 +66,13 @@ export const Media: CollectionConfig = {
         };
       },
     ],
+  },
+  access: {
+    read: async ({ req }) => {
+      return await isAdminOrHasAccessToImages()({ req });
+    },
+    delete: ({ req }) => {
+      return isAdminOrHasAccessToImages()({ req });
+    },
   },
 };
