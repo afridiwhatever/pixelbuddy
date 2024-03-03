ef2f9faa329240330fe2cc6018772e47f0893593
Afridi <afridiwhatever@gmail.com>
2024-03-04 03:34:05 +0600

    working on airbnb style slider

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "3001",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -5,38 +5,56 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
+import type SwiperType from "swiper";
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface ImageSliderProps {
   urls: string[];
 }
 
 const ImageSlider = ({ urls }: ImageSliderProps) => {
-  console.log(urls);
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+
   return (
-    <div>
+    <div className="relative aspect-square rounded-xl overflow-hidden group">
       <Swiper
-        pagination={{
-          renderBullet: (_, className) => {
-            return `<span class="rounded-full transition ${className}"></span>`;
-          },
-        }}
-        spaceBetween={50}
-        modules={[Pagination]}
-        slidesPerView={1}
         className="h-full w-full"
+        onSwiper={(swiper) => setSwiper(swiper)}
       >
         {urls.map((url, i) => (
-          <SwiperSlide key={i} className="-z-10 relative h-full w-full">
+          <SwiperSlide key={i} className="relative">
             <Image
               fill
               loading="eager"
-              className="-z-10 h-full w-full object-cover object-center"
+              className="object-cover object-center rounded-xl"
               src={url}
               alt="Product image"
             />
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {urls.length > 1 && (
+        <div className="absolute top-1/2 z-50 w-full px-2 flex justify-between -translate-y-1/2 invisible transition animate ease-in group-hover:visible">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              swiper?.slidePrev();
+            }}
+          >
+            <ChevronLeft className="h-8 w-8  p-2 bg-white rounded-full" />
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              swiper?.slideNext();
+            }}
+          >
+            <ChevronRight className="h-8 w-8 p-2 bg-white rounded-full" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
