e98d9e9fca656ddf743f38d08ed33f4c61332ac8
Afridi <afridiwhatever@gmail.com>
2024-03-18 08:51:39 +0600

    thank you page conditional logic

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,5 +1,47 @@
-const ThankYouPage = async () => {
-  return <>Hi</>;
-};
+import { getPayloadClient } from "@/payload/get-payload";
+import { cookies } from "next/headers";
+import { getServerSideUser } from "@/payload/payload-utils";
+import { notFound, redirect } from "next/navigation";
 
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const ThankYouPage = async ({ searchParams }: PageProps) => {
+  const orderId = searchParams.orderId;
+  const nextCookies = cookies();
+
+  const { user } = await getServerSideUser(nextCookies);
+  const payload = await getPayloadClient();
+
+  const { docs: orders } = await payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      id: {
+        equals: orderId,
+      },
+    },
+  });
+
+  const [order] = orders;
+
+  if (!order) return notFound();
+
+  const orderUserId =
+    typeof order.user === "string" ? order.user : order.user.id;
+
+  if (orderUserId !== user?.id) {
+    return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`);
+  }
+
+  return (
+    <p>
+      orderid {order.id}
+      userid {user.id}
+    </p>
+  );
+};
 export default ThankYouPage;
